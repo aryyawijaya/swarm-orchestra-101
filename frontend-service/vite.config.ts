3729a4ca7374938c34812260fdb334a0e572675f
Aryya Bagus Padmanawijaya <aryyawijaya@gmail.com>
2024-07-01 18:07:32 +0700

    feat: create Dockerfile & compose production for frontend service

--- frontend-service/vite.config.ts
+++ frontend-service/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
+const PORT = process.env.VITE_BACKEND_URL || '';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: Number(PORT),
+    host: true,
+    origin: `http://0.0.0.0:${PORT}`,
+  },
+});
